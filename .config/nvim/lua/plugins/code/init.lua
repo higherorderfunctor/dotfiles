b73f9d39d902545b9f0a32f9cb60bbca7093ff88
Christopher Aubut <christopher@aubut.me>
2023-12-14 18:59:03 -0700

    updates

--- .config/nvim/lua/plugins/code/init.lua
+++ .config/nvim/lua/plugins/code/init.lua
@@ -1,6 +1,18 @@
 return {
-  require("plugins.code.conform"),
-  require("plugins.code.copilot"),
-  require("plugins.code.eslint"),
-  require("plugins.code.luasnip"),
+	require("plugins.code.conform"),
+	require("plugins.code.copilot"),
+	require("plugins.code.eslint"),
+	require("plugins.code.luasnip"),
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			local cmp = require("cmp")
+			table.insert(opts, 1, {
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+			})
+		end,
+	},
 }
