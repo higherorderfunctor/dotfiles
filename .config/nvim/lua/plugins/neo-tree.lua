1f306de2d7c2eeb1b03a087b6f9480ac38ee9300
Christopher Aubut <christopher@aubut.me>
2024-01-31 11:29:33 -0700

    feat: update

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,33 +1,39 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	opts = {
-		default_component_configs = {
-			file_size = { enabled = false },
-			last_modified = { enabled = false },
-			created = { enabled = false },
-			symlink_target = { enabled = true },
-		},
-		buffers = {
-			follow_current_file = { enabled = true, leave_open = true },
-		},
-		filesystem = {
-			filtered_items = { visible = true },
-			follow_current_file = { enabled = true, leave_open = true },
-			window = {
-				mappings = {
-					["O"] = "system_open",
-				},
-			},
-			commands = {
-				system_open = function(state)
-					local node = state.tree:get_node()
-					local path = node:get_id()
-					-- macOs: open file in default application in the background.
-					vim.fn.jobstart({ "xdg-open", "-g", path }, { detach = true })
-					-- Linux: open file in default application
-					vim.fn.jobstart({ "xdg-open", path }, { detach = true })
-				end,
-			},
-		},
-	},
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    default_component_configs = {
+      file_size = { enabled = false },
+      last_modified = { enabled = false },
+      created = { enabled = false },
+      symlink_target = { enabled = true },
+    },
+    buffers = {
+      follow_current_file = { enabled = true, leave_open = true },
+    },
+    filesystem = {
+      filtered_items = { visible = true },
+      follow_current_file = { enabled = true, leave_open = true },
+      window = {
+        mappings = {
+          ["O"] = "system_open",
+        },
+      },
+      commands = {
+        -- move deleted files to the trash
+        delete = function(state)
+          local path = state.tree:get_node().path
+          vim.fn.system({ "gio", "trash", vim.fn.fnameescape(path) })
+          require("neo-tree.sources.manager").refresh(state.name)
+        end,
+        system_open = function(state)
+          local node = state.tree:get_node()
+          local path = node:get_id()
+          -- macOs: open file in default application in the background.
+          vim.fn.jobstart({ "xdg-open", "-g", path }, { detach = true })
+          -- Linux: open file in default application
+          vim.fn.jobstart({ "xdg-open", path }, { detach = true })
+        end,
+      },
+    },
+  },
 }
