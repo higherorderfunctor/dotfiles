0effd5b5117cc8bf82d965cddf8b5a51fecc7cca
Christopher Aubut <christopher@aubut.me>
2024-02-23 19:25:43 -0700

    updates

--- .config/nvim/lua/plugins/code/conform.lua
+++ .config/nvim/lua/plugins/code/conform.lua
@@ -1,13 +1,14 @@
 return {
-  "stevearc/conform.nvim",
-  opts = {
-    format = { lsp_fallback = "always" },
-    formatters_by_ft = {
-      html = { "prettierd" },
-      sh = { "shfmt", "beautysh" },
-      nix = { "alejandra" },
-      ["*"] = { "trim_newlines", "trim_whitespace" },
-      ["_"] = { "trim_newlines", "trim_whitespace" },
-    },
-  },
+	"stevearc/conform.nvim",
+	opts = {
+		format = { lsp_fallback = "always" },
+		formatters_by_ft = {
+			html = { "prettierd" },
+			sh = { "shfmt", "beautysh" },
+			nix = { "alejandra" },
+			typescript = {},
+			["*"] = { "trim_newlines", "trim_whitespace" },
+			["_"] = { "trim_newlines", "trim_whitespace" },
+		},
+	},
 }
